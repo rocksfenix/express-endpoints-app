6484b5ea4d85af4cec2269f53a935da3d1af104e
Gerardo Gallegos <puxerop@gmail.com>
2020-02-17 10:11:05 -0600

    I added socket.io to update the endpoints without reload the page, and is inyected the endpoints's data for initial rendering

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import io from 'socket.io-client'
+import { Helmet } from 'react-helmet'
+
+const { endpoints: initialEndpoints, name, port } = window.__EXPRESS_ENDPOINTS_DATA__
 
 const App = () => {
+  const [endpoints, setEndPoints] = useState(initialEndpoints)
+
+  useEffect(() => {
+    const socket = io('http://localhost:7070')
+
+    socket.on('change-endpoints', (data) => {
+      setEndPoints(data)
+      console.log(data)
+    })
+  }, [])
+
   return (
     <div>
-      <h1>Hey works!</h1>
+      <Helmet>
+        <title>{name}</title>
+      </Helmet>
+      <h1>{name} {port} Count: {endpoints.length}</h1>
+      <ul>
+        {endpoints.map(endpoint => (
+          <li key={endpoint.path}>
+            <span>
+              {endpoint.path}
+            </span>
+            {endpoint.methods.map(method => (
+              <button key={method}>
+                {method}
+              </button>
+            ))}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,72 +1,60 @@
 const chalk = require('chalk')
-const listEndpoints = require('express-list-endpoints')
-const express = require('express')
+const app = require('express')()
 const getPort = require('get-port')
-const livereload = require('livereload')
+const path = require('path')
+const fs = require('fs')
+const http = require('http').createServer(app)
+const listEndpoints = require('express-list-endpoints')
 
 const DEFAULT_PORT = 7070
 
-const run = async (wrappedExpressApp, port) => {
-  const endPoints = listEndpoints(wrappedExpressApp)
-  const app = express()
-  const portSocket = 35728
+const io = require('socket.io')(http)
 
-  livereload.createServer({
-    port: portSocket
+const readFile = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (error, contents) => {
+    if (error) {
+      console.log(
+        chalk.red('Failed load the template app')
+      )
+      reject(error)
+      process.exit(1)
+    }
+
+    resolve(contents)
+  })
+})
+
+const run = async (wrappedExpressApp, { name = 'Express Endpoints App' }) => {
+  const endPoints = listEndpoints(wrappedExpressApp)
+
+  io.on('connection', function (socket) {
+    console.log('a user connected')
+    socket.emit('change-endpoints', endPoints)
   })
 
   // Will use 7070 if available, otherwise fall back to a random port
   const PORT = await getPort({ port: DEFAULT_PORT })
-  const host = 'localhost'
-  const reloadTimeout = 400
 
-  // Build html File
-  const htmlContens = `
-  <!DOCTYPE html>
-  <html lang='en'>
-  <head>
-    <meta charset='UTF-8'/>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
-    <meta http-equiv='X-UA-Compatible' content='ie=edge'/>
-    <title>Express Endpoints App</title>
-  </head>
-  <body>
-    <h1>Express Endpoints App</h1>
-    <div>
-      ${endPoints.map(endPoint => (
-        `<div>
-          ${endPoint.path}
-          ${endPoint.methods.map(method => (
-            `<button onclick="makeRequest('http://localhost:${port}${endPoint.path}', '${method}')">
-              ${method}
-            </button>`
-          ))}
-        </div>`
-      ))}
-    </div>
-    <script>
-      const makeRequest = async (path, method) => {
-        console.log(path, method)
-    
-        if(method === 'GET') {
-          const res = await fetch(path)
-          const data = await res.json()
-          console.log(data)
-        }
-      }
+  // index.html with react app compiled
+  const reatAppPath = path.resolve(__dirname, '../', 'app-client', 'dist', 'index.html')
 
-      document.write('<script src="//' + (location.host || '${host}').split(':')[0] + ':${portSocket}/livereload.js?snipver=1"></' + 'script>')
-      document.addEventListener('LiveReloadDisconnect', function() { setTimeout(function() { window.location.reload(); }, ${reloadTimeout}); })
-    </script>
-  </body>
-  </html>
-  `
+  const markdownRawReactApp = await readFile(reatAppPath)
 
-  app.get('*', (req, res) => {
-    res.send(htmlContens)
+  const mkReactApp = markdownRawReactApp.replace(
+    '{{{DATA WILL BE HERE}}}',
+    `<script>window.__EXPRESS_ENDPOINTS_DATA__ = {
+      endpoints: ${JSON.stringify(endPoints)},
+      port: ${PORT},
+      name: '${name}'
+    }</script>
+    `
+  )
+
+  app.get('/', (req, res) => {
+    res.send(mkReactApp)
   })
 
-  app.listen(PORT, () => {
+  http.listen(PORT, () => {
     console.log(
       chalk.cyan(`⚡  Enpoints App: http://localhost:${PORT}`)
     )
